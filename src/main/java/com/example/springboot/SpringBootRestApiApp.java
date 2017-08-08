a7c66b9cfcf5b7968ceeba82d2f132585ceba228
Robert Rosario <Robert Rosario@192.168.1.12>
2017-08-09 00:10:03 +0530

    Xml Request with namespace

--- src/main/java/com/example/springboot/SpringBootRestApiApp.java
+++ src/main/java/com/example/springboot/SpringBootRestApiApp.java
@@ -1,13 +1,27 @@
 package com.example.springboot;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 
 
 @SpringBootApplication(scanBasePackages={"com.example.springboot"})// same as @Configuration @EnableAutoConfiguration @ComponentScan combined
 public class SpringBootRestApiApp {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SpringBootRestApiApp.class, args);
+		
+		System.setProperty("javax.net.ssl.trustStore", new ClassPathResource("xsd/Request.xsd").getFile().getPath());
+		System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+		
 	}
+	
+	
+	
+	
 }
--- src/main/java/com/example/springboot/controller/RestApiController.java
+++ src/main/java/com/example/springboot/controller/RestApiController.java
@@ -1,5 +1,9 @@
 package com.example.springboot.controller;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,11 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springboot.interceptor.BasicAuthRestTemplate;
-import com.example.springboot.model.req.Request;
-import com.example.springboot.model.res.InsuranceAdditionalInformation;
-import com.example.springboot.model.res.MessageSegment;
-import com.example.springboot.model.res.Response;
-import com.example.springboot.model.res.ResponseHeader;
+import com.example.springboot.requestxsd.Request;
+import com.example.springboot.responsexsd.Response;
 
 @RestController
 @RequestMapping("/api")
@@ -23,47 +24,46 @@ public class RestApiController {
 	public static final Logger logger = LoggerFactory.getLogger(RestApiController.class);
 	public static final String REST_SERVICE_URI = "http://localhost:8080/SpringBootRestApi/api";
 
-	@RequestMapping(value = "/request/", method = RequestMethod.POST, produces = { "application/json",
-			"application/xml" })
+	@RequestMapping(value = "/request/", method = RequestMethod.POST, consumes={"application/json","application/xml" },produces = { "application/json","application/xml" })
 	public ResponseEntity<?> postRequest(@RequestBody Request request) {
 
+		marshaller(request);
 		BasicAuthRestTemplate restTemplate = new BasicAuthRestTemplate("admin", "admin");
-
+		
 		ResponseEntity<Response> res = restTemplate.postForEntity(REST_SERVICE_URI + "/response/", request,
 				Response.class);
-
+		
 		return res;
 	}
 
-	@RequestMapping(value = "/response/", method = RequestMethod.POST, produces = { "application/json",
-			"application/xml" })
+	@RequestMapping(value = "/response/", method = RequestMethod.POST, consumes={"application/json","application/xml" },produces = { "application/json","application/xml" })
 	public ResponseEntity<?> getResponse(@RequestBody Request request) {
 		logger.info("Creating User : {}", request);
+		
+		marshaller(request);
 
 		Response response = new Response();
-
-		ResponseHeader responseHeader = new ResponseHeader();
-		responseHeader.setDateOfService("20170627");
-		responseHeader.setHeaderResponseStatus("A");
-		responseHeader.setServiceProviderid("1629029251");
-		responseHeader.setTransactionCode("E1");
-		responseHeader.setTransactionCount("1");
-		responseHeader.setVersionReleaseNumber("DO");
-		MessageSegment messageSegment = new MessageSegment();
-		messageSegment
-				.setMessage("ERX128: COMMERCIAL ELIGIBILITY PARTNER HAS INDICATED PRIMARY COVERAGE AS MEDICARE PART D");
-		InsuranceAdditionalInformation insuranceAdditionalInformation = new InsuranceAdditionalInformation();
-		insuranceAdditionalInformation.setBenefitId("20170701");
-		insuranceAdditionalInformation.setcMSLowIncomeCostSharing("Y");
-		insuranceAdditionalInformation.setContractNumber("23893783");
-		insuranceAdditionalInformation.setFormularyId("1238347");
-		insuranceAdditionalInformation.setMedicarePartDCoverageCode("10");
-		insuranceAdditionalInformation.setNextMedicarePartDTerminationDate("20190701");
-		response.setResponseHeader(responseHeader);
-		response.setInsuranceAdditionalInformation(insuranceAdditionalInformation);
-		response.setMessageSegment(messageSegment);
-
+		response.setCode("436554");
+		response.setMessage("Success");
+		response.setUniqueId("436554");
+		String[] funResponse = {"Cheese", "Pepperoni", "Black Olives"};
+		response.setFunResponse(funResponse);
 		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
-
+public void marshaller(Request request){
+	JAXBContext jaxbContext;
+	try {
+		jaxbContext = JAXBContext.newInstance(Request.class);
+	
+    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+ 
+    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+     
+    //Marshal the employees list in console
+    jaxbMarshaller.marshal(request, System.out);
+	} catch (JAXBException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
 }
